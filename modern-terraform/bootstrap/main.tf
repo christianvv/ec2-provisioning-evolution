e6e20c6bef395d903be7926e1afc2d81e84e88a5
christianvv <cvillalvav@gmail.com>
2026-06-27 12:00:31 +0600

    feat: add S3 state bucket with versioning, encryption, and public access block

--- modern-terraform/bootstrap/main.tf
+++ modern-terraform/bootstrap/main.tf
@@ -8,6 +8,44 @@ terraform {
   }
 }
 
+# Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
+}
+
+# Create the S3 tf state Bucket and attributes
+resource "aws_s3_bucket" "tf_state_bucket" {
+  bucket = "epe-mt-terraform-state-01"
+
+  tags = {
+    Name    = "Terraform State Bucket"
+    Purpose = "ec2-provisioning-evolution remote state"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "tf_state_bucket_versioning" {
+  bucket = aws_s3_bucket.tf_state_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "sse_configuration" {
+  bucket = aws_s3_bucket.tf_state_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access_block" {
+  bucket = aws_s3_bucket.tf_state_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
